565c94f57b3bbe8c9054244cb40ab27e22ab306c
Joachim Grubbe Frank <jgf.easj@gmail.com>
2021-10-30 17:03:17 +0200

    Update MessageController.cs

--- MessageQueue/Controllers/MessageController.cs
+++ MessageQueue/Controllers/MessageController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
 
 namespace MessageQueue.Controllers
 {
@@ -31,25 +33,41 @@ namespace MessageQueue.Controllers
         //file name should be 4 random numbers + date and time down to milisecond (have epoch converter)
         [HttpPost]
         [Route("create")]
-        public string CreateMessage(string file)
+        public async string CreateMessage(string file)
         {
             //Masterfolder
-            string altFolderName = @"C:\Users\joach\Documents\KEA\Semester 2\System Integration\SI_Mandatory2\MessageQueue\Messages";
+            string altFolderPath = @"C:\Users\joach\Documents\KEA\Semester 2\System Integration\SI_Mandatory2\MessageQueue\Messages";
             //Folder for messages
-            string  folderName = 
+            string  folderPath = 
             System.IO.Path.Combine(System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).FullName, "Messages");
 
             //TopicFolder ------ Get the topic from the file read from the post message!!!!!------------------------
             string topic = "";
-            folderName = System.IO.Path.Combine(folderName, topic);
+            folderPath = System.IO.Path.Combine(folderPath, topic);
 
             //Check if folder exist, if not: create it
-            if(!System.IO.Directory.Exists(folderName)){
-                System.IO.Directory.CreateDirectory(folderName);
+            if(!System.IO.Directory.Exists(folderPath)){
+                System.IO.Directory.CreateDirectory(folderPath);
             }
             
-            
+            //Write name for file (4 random numbers + date + time + miliseconds)
+            var rand = new Random();
+            string numbers = "";
+            for(int i = 0; i<=3; i++)
+            {
+                numbers = numbers +rand.Next(10);
+            }
 
+            
+            string fileName = numbers + DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            
+            //put received string in file
+            await File.WriteAllText(fileName, file);
+            //Make sure file is saved in "folderPath"
+
+            //done
+
+            return "File Recorded";
         }
     }
 }
