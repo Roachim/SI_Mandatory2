f227b022d6fadec6f56af1a5d33bb3563842684b
Roachim <78924571+Roachim@users.noreply.github.com>
2021-11-08 17:15:22 +0100

    small update CSV

--- MessageQueue/Converters/CSVConverter.cs
+++ MessageQueue/Converters/CSVConverter.cs
@@ -4,20 +4,51 @@ using System.IO;
 using System.Globalization;
 using System.Threading;
 using System.Collections;
+using System.Text;
 
 namespace MessageQueue
 {
     public static class CSVConverter
     {
-        private static IEnumerable DeserializeCSV(string CSV)
+        public static IEnumerable DeserializeCSV(string PathToCSVFile)
         {
 
-            TextReader reader = new StreamReader(CSV);
+            TextReader reader = new StreamReader(PathToCSVFile);
             var csvReader = new CsvReader(reader, CultureInfo.CurrentCulture);
             
             var records = csvReader.GetRecords<dynamic>();
 
             return records;
         }
+        public static string SerializeCSV(dynamic records, string pathToMessages){
+         
+            var csv = new StringBuilder();
+
+            using var textWriter = new StringWriter(csv);
+            using var csvWriter = new CsvWriter(textWriter, CultureInfo.CurrentCulture);
+
+            // automatically map the properties of T
+            csvWriter.Configuration.AutoMap<T>();
+
+            // write our record
+            csvWriter.WriteRecord(input);
+
+            // make sure all records are flushed to stream
+            await csvWriter.FlushAsync();
+
+            Console.WriteLine(csv.ToString());
+           
+        }
+        // public static string SerializeCSV(dynamic records, string pathToMessages){
+        //     string CSVDUMPPath = System.IO.Path.Combine(pathToMessages, "CSVDump.CSV");
+        //     System.IO.Directory.CreateDirectory(CSVDUMPPath);
+        //     System.IO.File.WriteAllText(CSVDUMPPath, "");
+
+        //     using (var writer = new StreamWriter(CSVDUMPPath))
+        //     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        //     {               
+        //         csv.WriteRecords(records);
+        //     }
+        // }
     }
 }
