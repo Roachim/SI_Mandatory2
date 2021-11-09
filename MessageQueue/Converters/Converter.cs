ff422b36e52f12223cb54607a33be2e57160efb0
Andu972k <heyanders@gmail.com>
2021-11-09 13:03:59 +0100

    Added a few comments

--- MessageQueue/Converters/Converter.cs
+++ MessageQueue/Converters/Converter.cs
@@ -15,8 +15,10 @@ namespace MessageQueue.Converters
         //Json can then become any type ( number of formats * 1(JSON) + 1(JSON) * number of converters = number of converters)
         //3. Have a method that registers a format and converts to the then wished for format. (1 converter)
         public static string Convert(string filePath, string toFormat){
-            //Find the format of the file by looking after the '.'
-            //get path to messages
+            //Gets a collection with the unread messages, and their format
+            //Also needs the format that the messages should be converted to
+            //Sends a request to an external api for conversion
+            //Returns a list of the converted messages
         }
         
         
